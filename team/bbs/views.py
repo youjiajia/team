2e801a2a9148836ba11d35607f8624fffe89a83a
youjiajia <hi_youjiajia@163.com>
2016-03-13 18:03:27 +0800

    sd

--- team/bbs/views.py
+++ team/bbs/views.py
@@ -61,6 +61,7 @@ class MemoTemplateView(TemplateView):
             urlresponse = urllib2.urlopen(urlreq)
             the_page = urlresponse.read()
             jsonreturn=json.loads(the_page)
+            print jsonreturn
             if jsonreturn.has_key('UserId'):
                 if T_Member.objects.filter(UserID=jsonreturn['UserId'],IsUsed=True).count()==0:
                     T_Member.objects.create(UserID=jsonreturn['UserId'],IsUsed=True)
